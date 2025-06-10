c26c9aeacc05ae7006ba9818786cd88b6bd36b3d
Spoorthy K R <spoorthykr1801@gmail.com>
2025-06-10 21:51:57 +0530

    added achievement component

---
+++ src/components/achievement/Achievement.js
@@ -0,0 +1,98 @@
+import React, {useContext} from "react";
+import "./Achievement.css";
+import {achievementSection} from "../portfolio";
+import {Fade} from "react-awesome-reveal";
+import StyleContext from "../../contexts/StyleContext";
+
+function AchievementCard({cardInfo}) {
+  function openUrlInNewTab(url, name) {
+    if (!url) {
+      console.log(`URL for ${name} not found`);
+      return;
+    }
+    var win = window.open(url, "_blank");
+    win.focus();
+  }
+
+  return (
+    <div className={"certificate-card"}>
+      <div className="certificate-image-div">
+        <img
+          src={cardInfo.image}
+          alt={cardInfo.imageAlt || "Card Thumbnail"}
+          className="card-image"
+        ></img>
+      </div>
+      <div className="certificate-detail-div">
+        <h5 className={"card-title"}>
+          {cardInfo.title}
+        </h5>
+        <p className={"card-subtitle"}>
+          {cardInfo.description}
+        </p>
+      </div>
+      <div className="certificate-card-footer">
+        {cardInfo.footer.map((v, i) => {
+          return (
+            <span
+              key={i}
+              className={"certificate-tag"
+              }
+              onClick={() => openUrlInNewTab(v.url, v.name)}
+            >
+              {v.name}
+            </span>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default function Achievement() {
+  // const {isDark} = useContext(StyleContext);
+  if (!achievementSection.display) {
+    return null;
+  }
+  return (
+    <Fade bottom duration={1000} distance="20px">
+      <div className="main" id="achievements">
+        <div className="achievement-main-div">
+          <div className="achievement-header">
+            <h1
+              className={
+                "heading achievement-heading"
+              }
+            >
+              {achievementSection.title}
+            </h1>
+            <p
+              className={
+                "subTitle achievement-subtitle"
+              }
+            >
+              {achievementSection.subtitle}
+            </p>
+          </div>
+          <div className="achievement-cards-div">
+            {achievementSection.achievementsCards.map((card, i) => {
+              return (
+                <AchievementCard
+                  key={i}
+                  // isDark={isDark}
+                  cardInfo={{
+                    title: card.title,
+                    description: card.subtitle,
+                    image: card.image,
+                    imageAlt: card.imageAlt,
+                    footer: card.footerLink
+                  }}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </Fade>
+  );
+}
